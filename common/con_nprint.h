a40c10e0d1fa43aa644bab65f9f21d5f191ea44a
bariscodefx <bariscodefx@protonmail.com>
2024-08-12 00:26:52 +0300

    client: fix compiling

--- common/con_nprint.h
+++ common/con_nprint.h
@@ -1,17 +1,17 @@
 /***
-*
-*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
-*	
-*	This product contains software technology licensed from Id 
-*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
-*	All Rights Reserved.
-*
-*   Use, distribution, and modification of this source code and/or resulting
-*   object code is restricted to non-commercial enhancements to products from
-*   Valve LLC.  All other use, distribution, or modification is prohibited
-*   without written permission from Valve LLC.
-*
-****/
+ *
+ *	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+ *
+ *	This product contains software technology licensed from Id
+ *	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+ *	All Rights Reserved.
+ *
+ *   Use, distribution, and modification of this source code and/or resulting
+ *   object code is restricted to non-commercial enhancements to products from
+ *   Valve LLC.  All other use, distribution, or modification is prohibited
+ *   without written permission from Valve LLC.
+ *
+ ****/
 #if !defined( CON_NPRINTH )
 #define CON_NPRINTH
 #ifdef _WIN32
@@ -22,16 +22,25 @@
 
 typedef struct con_nprint_s
 {
-	int		index;			// Row #
-	float	time_to_live;	// # of seconds before it disappears
-	float	color[ 3 ];		// RGB colors ( 0.0 -> 1.0 scale )
+	int index;          // Row #
+	float time_to_live; // # of seconds before it disappears
+	float color[3];     // RGB colors ( 0.0 -> 1.0 scale )
 } con_nprint_t;
+
+#ifdef CL_DLL_H
+
+void Con_NPrintf( int idx, char *fmt, ... );
+void Con_NXPrintf( struct con_nprint_s *info, char *fmt, ... );
+
+#else
 
 #ifndef Con_NPrintf
 #define Con_NPrintf ( *EngFuncs::engfuncs.Con_NPrintf )
 #endif
 #ifndef Con_NXPrintf
 #define Con_NXPrintf ( *EngFuncs::engfuncs.Con_NXPrintf )
+#endif
+
 #endif
 
 #endif
